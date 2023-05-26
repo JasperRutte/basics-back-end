3f365dd587c1f49f2131c520951f4b50061c75f1
Jasper Rutte <jasperrutte@gmail.com>
2023-05-26 19:45:47 +0200

    assignment sort array without sort()

--- php-functions/opdracht3.php
+++ php-functions/opdracht3.php
@@ -1,11 +1,26 @@
 <?php
-function sort_numbers($num_array)
+$arr = ['2','4','8','5','1','7','6','9','10','3'];
+
+
+function sort_array(array $num_array)
 {
-    sort($num_array);
-    foreach ($num_array as $num){
-        echo "<br>",$num;
+    $sorted_array = [];
+    foreach ($num_array as $number){
+        $smallest_number = min($num_array);
+        $sorted_array[] = $smallest_number;
+        $remove_num = array_search($smallest_number, $num_array);
+        unset($num_array[$remove_num]);
+    }
+    foreach ($sorted_array as $numb){
+        echo $numb, "<br>";
     }
 }
 
-$nums = [3, 4, 1, 6, 8, 9];
-sort_numbers($nums);
+
+
+//function sort_array(array $num_array)
+//{
+
+//}
+
+sort_array($arr);
