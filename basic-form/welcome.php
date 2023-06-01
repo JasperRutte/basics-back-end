3dc4ed77ae1c4278041ad781ad0729a9e94e127b
Jasper Rutte <jasperrutte@gmail.com>
2023-06-01 15:04:44 +0200

    welcome.php assignment

--- basic-form/welcome.php
+++ basic-form/welcome.php
@@ -3,35 +3,61 @@
 <head>
     <meta charset="UTF-8">
     <title>Welcome</title>
+    <style>
+        .error {color: #FF0000;}
+    </style>
 </head>
 <body>
 
 <?php
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
 function test_input($data) {
     $data = trim($data);
     $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
+    return htmlspecialchars($data);
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-$name = test_input($_POST["name"]);
-$email = test_input($_POST["email"]);
+    if (empty($_POST["name"])) {
+        $nameErr = "Name is required";
+    } else {
+        $name = test_input($_POST["name"]);
+        // check if name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+            $nameErr = "Only letters and white space allowed";
+        }
+    }
+
+    if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+    } else {
+        $email = test_input($_POST["email"]);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "Invalid email format";
+        }
+    }
 }
 ?>
 
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-    Name: <label><input type="text" name="name"></label>
+    Name: <input type="text" name="name">
+    <span class="error">* <?php echo $nameErr;?></span>
     <br><br>
-    E-mail: <label><input type="text" name="email"></label>
-    <input type="submit">
+    E-mail: <input type="text" name="email">
+    <span class="error">* <?php echo $emailErr;?></span>
+    <br><br>
+    <input type="submit" name="submit" value="Submit">
 </form>
 
 <?php
 echo "<h2>Your Input:</h2>";
-echo $name, "<br>";
+echo $name;
+echo "<br>";
 echo $email;
 ?>
+
+
 
 </body>
 </html>
